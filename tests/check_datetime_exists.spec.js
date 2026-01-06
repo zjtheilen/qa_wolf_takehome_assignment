4adbfcde15ec5f87950f75d560deda1187b292c5
zjtheilen <zjtheilen@gmail.com>
2026-01-06 12:15:08 -0800

    revert to project default to focus testing in index.js

--- tests/check_datetime_exists.spec.js
+++
@@ -1,8 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('check datetime exists', async ({ page }) => {
-  await page.goto('https://news.ycombinator.com/');
-  const ageSpan = page.locator('span.age').first();
-  const title = await ageSpan.getAttribute('title');
-  expect(title).not.toBeNull();
-});
